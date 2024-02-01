de853f275a1e01fb7c58b4dca96d6b8f8dcda75e
Michał Kopeć <michal@nozomi.space>
2024-02-01 03:08:06 +0100

    entrypoint.sh: debug errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
 
+ls
+
 WORKPATH=/workspace/michal/$INPUT_PATH
 cd $WORKPATH
+
+ls
+ls ..
 
 # HACK: Trick makepkg into running as root.
 export FAKEROOTKEY=1
 
 makepkg -F --printsrcinfo >.SRCINFO
-git diff .SRCINFO
